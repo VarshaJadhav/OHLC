77a2a7c5b25fb7b1129d06a63eea577440d60912
Varsha Jadhav <vjadhav@instarem.com>
2018-09-23 20:17:02 +0530

    socket connection configured

--- src/components/live-ohlc/live-ohlc.js
+++ src/components/live-ohlc/live-ohlc.js
@@ -1,7 +1,25 @@
 // React modules
 import React, { Component } from 'react';
+import io from 'socket.io-client';
+
+import { ROOT_URL } from '../../constants';
+
+/* make socket connection with our root/server URL */
+const socket = io(ROOT_URL, {
+  'reconnection': true,
+  'reconnectionDelay': 1000,
+  'reconnectionDelayMax': 5000,
+  'forceNew': true
+});
 
 class LiveOhlc extends Component {
+  componentWillMount() {
+    socket.on(`subscribe`, data => {
+      console.log({ data })
+    });
+  }
+
+
   render() {
     return (
       <div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import { HEADER_NAV } from './header-nav-options';
 import { USER_DATA } from './user-data';
+import { ROOT_URL } from './config';
 
 export  {
   HEADER_NAV,
-  USER_DATA
+  USER_DATA,
+  ROOT_URL
 };
