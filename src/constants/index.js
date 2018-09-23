b15ad9ac4e6ae8465a5a88c557f34d53a38c07e6
Varsha Jadhav <vjadhav@instarem.com>
2018-09-23 18:38:57 +0530

    constant created for search data

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,7 @@
 import { HEADER_NAV } from './header-nav-options';
-
+import { USER_DATA } from './user-data';
 
 export  {
-  HEADER_NAV
+  HEADER_NAV,
+  USER_DATA
 };
---
+++ src/constants/user-data.js
@@ -0,0 +1,60 @@
+export const USER_DATA = [
+  {
+    "_id": "5b3a0ed0e5ca3ddf07ad9062",
+    "isActive": true,
+    "balance": "$2,681.21",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "name": {
+      "first": "Patel",
+      "last": "Romero"
+    },
+    "company": "Mercedes",
+    "email": "pate.romero@mercedes.net",
+    "phone": "+1 (984) 519-2280",
+    "address": {
+      "house_number": 923,
+      "street": "Havens Place",
+      "city": "Bannock",
+      "state": "Texas",
+      "pincode": 8764
+    },
+    "registered": "Tuesday, December 1, 2015 9:34 AM",
+    "latitude": "34.95546",
+    "longitude": "40.321145",
+    "tags": [
+      "consequat",
+      "officia",
+      "do"
+    ]
+  },
+  {
+    "_id": "5b3a0ed0e5ca3ddf07a000000",
+    "isActive": true,
+    "balance": "$2,681.21",
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "name": {
+      "first": "varsha",
+      "last": "jadhav"
+    },
+    "company": "Yercedes",
+    "email": "paadsdte.ggdf@rr.net",
+    "phone": "+1 (984) 519-2280",
+    "address": {
+      "house_number": 4322,
+      "street": "Havenrers Placrerere",
+      "city": "erette",
+      "state": "fgfgfg",
+      "pincode": 43434
+    },
+    "registered": "Tuesday, December 1, 2015 9:34 AM",
+    "latitude": "34.95546",
+    "longitude": "40.321145",
+    "tags": [
+      "ueiett",
+      "fdjhgjfdg",
+      "poort"
+    ]
+  } 
+]
